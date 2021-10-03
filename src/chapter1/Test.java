966b29d84f8758cda226bde3a3494239ceceaf51
alexhan2021 <alex.han2021@hotmail.com>
2021-10-03 22:57:21 +0800

    add demo

--- src/chapter1/Test.java
+++ src/chapter1/Test.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("This is a Test");
     }
 }
