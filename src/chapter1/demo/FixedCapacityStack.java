76fa99ae3501acf0902bc429a2079756c0a5f5e5
Yainsun <yainsun@qq.com>
2021-10-03 21:58:31 +0800

    add demo

--- src/chapter1/demo/FixedCapacityStack.java
+++ src/chapter1/demo/FixedCapacityStack.java
@@ -1,9 +1,10 @@
 package chapter1.demo;
 
 public class FixedCapacityStack<Item> {
-    private final Item[] a;
+    private Item[] a;
     private int N;
 
+    @SuppressWarnings("unchecked")
     public FixedCapacityStack(int cap) {
         a = (Item[]) new Object[cap];
     }
@@ -17,10 +18,19 @@ public class FixedCapacityStack<Item> {
     }
 
     public void push(Item item) {
+        if (N == a.length) resize(a.length * 2);
         a[N++] = item;
     }
 
     public Item pop() {
+        if (N > 0 && N == a.length / 4) resize(a.length / 2);
         return a[--N];
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize(int max) {
+        Item[] tmp = (Item[]) new Object[max];
+        System.arraycopy(a, 0, tmp, 0, N);
+        a = tmp;
     }
 }
