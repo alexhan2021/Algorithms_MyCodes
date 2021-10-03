0010f11101ad4522f3f54e75076ba81653d39ca7
Yainsun <yainsun@qq.com>
2021-10-03 21:21:11 +0800

    add demo

--- src/chapter1/Test.java
+++ src/chapter1/Test.java
@@ -1,5 +1,7 @@
 package chapter1;
 
+import java.util.ArrayList;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test");
---
+++ src/chapter1/demo/FixedCapacityStack.java
@@ -0,0 +1,26 @@
+package chapter1.demo;
+
+public class FixedCapacityStack<Item> {
+    private final Item[] a;
+    private int N;
+
+    public FixedCapacityStack(int cap) {
+        a = (Item[]) new Object[cap];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        a[N++] = item;
+    }
+
+    public Item pop() {
+        return a[--N];
+    }
+}
