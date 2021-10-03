1fa36bdc5a14ef716bf5146ed064798222e433b0
Yainsun <yainsun@qq.com>
2021-10-03 22:45:27 +0800

    add demo

---
+++ src/chapter1/demo/Stack.java
@@ -0,0 +1,41 @@
+package chapter1.demo;
+
+import java.util.Iterator;
+
+public class Stack<Item> implements Iterable<Item> {
+    private Node first;
+    private int N;
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+}
